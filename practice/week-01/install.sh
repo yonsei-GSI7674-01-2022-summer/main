d37f6ce107e1852752e61c260da37a77b81e66fd
jw-temp <wsbtgt@gmail.com>
2022-05-21 02:12:57 +0900

    update

--- practice/week-01/install.sh
+++ practice/week-01/install.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
 sudo apt install python3.8 -y
 sudo apt install python3.8-venv -y
-python3.8 -m venv venv
-. venv/bin/activate
+python3.8 -m venv ../../venv
+. ../../venv/bin/activate
 pip install -U pip
 pip install -r requirements.txt
